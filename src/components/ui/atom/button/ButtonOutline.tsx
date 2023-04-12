02d8dc380a677711601aa76475b3c67299859cb6
thangmaiduc <thangmaiduc00@gmail.com>
2023-04-12 14:52:39 +0700

    a little navbar, Form register, list item for link footer

--- src/components/ui/atom/button/ButtonOutline.tsx
+++ src/components/ui/atom/button/ButtonOutline.tsx
@@ -1,15 +1,17 @@
 import { Button } from '@mui/material'
 import * as React from 'react'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import { size } from '@/utils'
 
 export interface IButtonOutlineProps {
   children: React.ReactNode
+  className?: string
 }
 
 export default function ButtonOutline(props: IButtonOutlineProps) {
   return (
     <Button
-      className=' hover:bg-red-600 px-8 hover:text-white text-red-600'
+      className={` hover:bg-red-600 px-8 hover:text-white text-red-600 ${props.className}`}
       variant='outlined'
       size='medium'
     >
--- src/components/ui/layout/Footer.tsx
+++ src/components/ui/layout/Footer.tsx
@@ -1,18 +1,110 @@
-import { Box, Stack, Typography } from '@mui/material'
+import {
+  Box,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  TextField,
+  ListItemText,
+  Stack,
+  Typography,
+  Divider
+} from '@mui/material'
+import Link from 'next/link'
 import * as React from 'react'
-
+import FormRegister from '../molecules/FormRegister'
+// import
 export interface IFooterProps {}
+export const MENU_ITEMS = [
+  {
+    title: 'Solution',
+    children: {
+      data: [
+        {
+          title: 'Item Description 1',
+          to: 'description1'
+        },
+        {
+          title: 'Item Description 1',
+          to: 'description1'
+        },
+        {
+          title: 'Item Description 1',
+          to: 'description1'
+        }
+      ]
+    }
+  },
+  {
+    title: 'Solution2',
+    children: {
+      data: [
+        {
+          title: 'Item Description 1',
+          to: 'description2'
+        },
+        {
+          title: 'Item Description 1',
+          to: 'description2'
+        },
+        {
+          title: 'Item Description 1',
+          to: 'description2'
+        }
+      ]
+    }
+  },
+  {
+    title: 'Solution3',
+    children: {
+      data: [
+        {
+          title: 'Item Description 1',
+          to: 'description3'
+        },
+        {
+          title: 'Item Description 1',
+          to: 'description3'
+        },
+        {
+          title: 'Item Description 1',
+          to: 'description3'
+        }
+      ]
+    }
+  }
+]
 
 export default function Footer(props: IFooterProps) {
   return (
-    <Box>
-      <Stack direction={'row'} spacing={4}>
-        <Box width={1 / 4}>Item1</Box>
-        <Box>Item2</Box>
-        <Box>Item3</Box>
-        <Typography>{'thang '}</Typography>
-      </Stack>
-      <Box></Box>
-    </Box>
+    <Grid container padding={8} bgcolor={'black'} color={'white'}>
+      <Grid container item md={8} spacing={4}>
+        {MENU_ITEMS.map((item, index) => (
+          <Grid key={index} item md={3} xs={4}>
+            <Typography>{item.title}</Typography>
+            <List>
+              {item.children.data.map((child, idx) => (
+                <Link
+                  key={idx}
+                  href={child.to}
+                  passHref
+                  className='text-sm hover:text-white text-gray-500'
+                >
+                  <ListItem>{child.title}</ListItem>
+                </Link>
+              ))}
+            </List>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid item md={4}>
+        <Typography variant='h4'>Stay in the know with our newsletter</Typography>
+        <FormRegister />
+      </Grid>
+      <Grid item md={12} className='pt-4 border-t-2 mt-4 '>
+        <Typography>2023 Fast Tech</Typography>
+        <Typography>Part of Nash Squared.</Typography>
+      </Grid>
+    </Grid>
   )
 }
