e0acf9d2b8e86b4034ae3e1276bb19dbfbb718ff
thangmaiduc <thangmaiduc00@gmail.com>
2023-04-12 16:48:57 +0700

    create component list articles and article

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,14 @@
 import DefaultLayout from '@/components/ui/layout/DefaultLayout'
+import LayoutSidebar from '@/components/ui/layout/LayoutSidebar'
+import Article from '@/components/ui/molecules/Article'
+import ListArticles from '@/components/ui/organisms/ListArticles'
 import { Box } from '@mui/material'
 import React from 'react'
 
 export default function index() {
-  return <DefaultLayout />
+  return (
+    <DefaultLayout>
+      <ListArticles />
+    </DefaultLayout>
+  )
 }
